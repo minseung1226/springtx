83998131be60dd5223fd324ce7edff7283f38803
minseung1226 <minseung1226@naver.com>
2023-01-26 23:21:21 +0900

    스프링 트랜잭션 이해 -스프링 트랜잭션 소개 ,프록시 내부호출의 문제점, 트랜잭션 초기화 시점, 예외와 트랜잭션 커밋,롤백

---
+++ src/main/java/hello/springtx/order/NotEnoughtMoneyException.java
@@ -0,0 +1,7 @@
+package hello.springtx.order;
+
+public class NotEnoughtMoneyException extends Exception{
+    public NotEnoughtMoneyException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/hello/springtx/order/OrderServiceTest.java
@@ -0,0 +1,59 @@
+package hello.springtx.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Slf4j
+class OrderServiceTest {
+
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository repository;
+
+    @Test
+    void order() throws NotEnoughtMoneyException {
+        Order order=new Order();
+        order.setUsername("정상");
+
+        orderService.order(order);
+
+        Order findOrder = repository.findById(order.getId()).get();
+
+        assertThat(findOrder.getPayment()).isEqualTo("완료");
+    }
+
+    @Test
+    void runtimeException() throws NotEnoughtMoneyException {
+        Order order=new Order();
+        order.setUsername("예외");
+
+        assertThatThrownBy(()->orderService.order(order)).isInstanceOf(RuntimeException.class);
+
+        Optional<Order> orderOptional = repository.findById(order.getId());
+
+        assertThat(orderOptional.isEmpty()).isTrue();
+    }
+
+    @Test
+    void bizException(){
+        Order order=new Order();
+        order.setUsername("잔고 부족");
+
+        try {
+            orderService.order(order);
+        } catch (NotEnoughtMoneyException e) {
+            log.info("고객에게 잔고부족을 알리고 별도 계좌 안내");
+        }
+
+        Order findOrder = repository.findById(order.getId()).get();
+
+        assertThat(findOrder.getPayment()).isEqualTo("대기");
+    }
+}
